30f8c31a637e04befa51423cddf1d821fde53d0b
Nicole <nicole.georgievva@gmail.com>
2026-02-12 18:47:31 +0200

    Improve ComposeViewModel's documentation

--- app/src/main/java/com/example/app/common/ComposeViewModel.kt
+++ app/src/main/java/com/example/app/common/ComposeViewModel.kt
@@ -3,9 +3,9 @@ package com.example.app.common
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.ViewModel
 
-/*
-Should be used in the ViewModels of the screens.
-It's a must to override the uiState and onEvent fun - return uiState and handle events.
+/**
+Should be inherited by the ViewModels of the screens.
+It's a must to override the uiState() and onEvent() fun - return UI State and handle events.
  */
 abstract class ComposeViewModel<S, E> : ViewModel() {
   @Composable
