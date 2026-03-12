943fbfdbfd9b4eac4f710f4e7625152999f2b7d3
Nicole <nicole.georgievva@gmail.com>
2026-03-12 16:10:01 +0200

    Implement Cryptography

--- app/src/main/java/com/example/app/data/cryptography/AndroidKeystore.kt
+++ app/src/main/java/com/example/app/data/cryptography/AndroidKeystore.kt
@@ -2,6 +2,8 @@ package com.example.app.data.cryptography
 
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.security.KeyStore
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
@@ -13,29 +15,31 @@ class AndroidKeystore @Inject constructor() {
     private const val KEY_ALIAS = "secret_key"
   }
 
-  fun getSecretKey(): SecretKey {
-    val keyStore = KeyStore.getInstance(PROVIDER).apply { load(null) }
+  suspend fun getSecretKey(): SecretKey {
+    return withContext(Dispatchers.IO) {
+      val keyStore = KeyStore.getInstance(PROVIDER).apply { load(null) }
 
-    val existingKey = keyStore.getKey(KEY_ALIAS, null)
-    if (existingKey != null) {
-      return existingKey as SecretKey
+      val existingKey = keyStore.getKey(KEY_ALIAS, null)
+      if (existingKey != null) {
+        return@withContext existingKey as SecretKey
+      }
+
+      val keyGenerator = KeyGenerator.getInstance(
+        KeyProperties.KEY_ALGORITHM_AES,
+        PROVIDER
+      )
+
+      val spec = KeyGenParameterSpec.Builder(
+        KEY_ALIAS,
+        KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+      )
+        .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+        .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+        .setKeySize(256)
+        .build()
+      keyGenerator.init(spec)
+
+      keyGenerator.generateKey()
     }
-
-    val keyGenerator = KeyGenerator.getInstance(
-      KeyProperties.KEY_ALGORITHM_AES,
-      PROVIDER
-    )
-
-    val spec = KeyGenParameterSpec.Builder(
-      KEY_ALIAS,
-      KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
-    )
-      .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
-      .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
-      .setKeySize(256)
-      .build()
-    keyGenerator.init(spec)
-
-    return keyGenerator.generateKey()
   }
 }
