82fa18f8fd6b4e69e21509d484ca73101bcee0e2
Nicole <nicole.georgievva@gmail.com>
2026-03-12 16:19:16 +0200

    Support access token encryption

--- app/src/main/java/com/example/app/data/datastore/SessionStorage.kt
+++ app/src/main/java/com/example/app/data/datastore/SessionStorage.kt
@@ -1,6 +1,7 @@
 package com.example.app.data.datastore
 
 import androidx.datastore.preferences.core.edit
+import com.example.app.data.cryptography.Cryptography
 import com.example.app.di.LocalDataStore
 import com.example.app.domain.model.Session
 import kotlinx.coroutines.flow.first
@@ -9,10 +10,13 @@ import javax.inject.Inject
 
 class SessionStorage @Inject constructor(
   private val dataStore: LocalDataStore,
+  private val cryptography: Cryptography,
 ) {
   suspend fun get(): Session? {
     return dataStore.data.map {
-      val accessToken = it[DataStoreKeys.accessToken]
+      val accessToken = it[DataStoreKeys.accessToken]?.let { encryptedAccessToken ->
+        cryptography.decrypt(encryptedAccessToken).getOrNull()
+      }
       if (accessToken != null) {
         Session(accessToken = accessToken)
       } else {
@@ -23,7 +27,7 @@ class SessionStorage @Inject constructor(
 
   suspend fun store(session: Session) {
     dataStore.edit {
-      it[DataStoreKeys.accessToken] = session.accessToken
+      it[DataStoreKeys.accessToken] = cryptography.encrypt(session.accessToken)
     }
   }
 
