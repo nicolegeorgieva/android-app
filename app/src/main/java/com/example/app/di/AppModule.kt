6de1c47c53e076bc355e0d7377aabf592e34fa0f
Nicole <nicole.georgievva@gmail.com>
2026-03-13 13:56:15 +0200

    Extract a KtorModule

--- app/src/main/java/com/example/app/di/AppModule.kt
+++ app/src/main/java/com/example/app/di/AppModule.kt
@@ -5,7 +5,6 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.room.Room
-import com.example.app.MainEventBus
 import com.example.app.data.database.MyAppDatabase
 import com.example.app.data.database.task.TaskDao
 import com.example.app.data.datasource.login.FakeLoginDataSource
@@ -13,93 +12,19 @@ import com.example.app.data.datasource.login.LoginDataSource
 import com.example.app.data.datasource.task.FakeTaskRemoteDataSource
 import com.example.app.data.datasource.task.TaskRemoteDataSource
 import com.example.app.data.datastore.SessionStorage
-import com.example.app.domain.LogoutUseCase
-import com.example.app.utils.Logger
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import io.ktor.client.HttpClient
-import io.ktor.client.plugins.DefaultRequest
-import io.ktor.client.plugins.auth.Auth
-import io.ktor.client.plugins.auth.providers.BearerTokens
-import io.ktor.client.plugins.auth.providers.bearer
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.logging.LogLevel
-import io.ktor.client.plugins.logging.Logging
-import io.ktor.client.request.HttpRequestBuilder
-import io.ktor.http.ContentType
-import io.ktor.http.contentType
-import io.ktor.serialization.kotlinx.json.json
-import io.ktor.util.AttributeKey
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.contextual
-import javax.inject.Singleton
 import kotlin.time.ExperimentalTime
-import io.ktor.client.plugins.logging.Logger as KtorLogger
-
-private val AuthenticatedAttributeKey = AttributeKey<Boolean>("authenticated-attribute")
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-  @Provides
-  @Singleton
-  fun provideHttpClient(
-    json: Json,
-    sessionStorage: SessionStorage,
-    logoutUseCase: LogoutUseCase,
-    mainEventBus: MainEventBus,
-    appLogger: Logger,
-  ): HttpClient {
-    return HttpClient {
-      install(Logging) {
-        level = LogLevel.ALL
-        logger = object : KtorLogger {
-          override fun log(message: String) {
-            appLogger.debug("KtorClient") { message }
-          }
-        }
-      }
-      install(ContentNegotiation) {
-        json(
-          json = json,
-          contentType = ContentType.Any
-        )
-      }
-      install(DefaultRequest) {
-        contentType(ContentType.Application.Json)
-        url(urlString = "https://api.baubuddy.de/")
-      }
-      install(Auth) {
-        bearer {
-          // If true, sends the auth bearer header without waiting for 401 response
-          sendWithoutRequest { request ->
-            request.isAuthenticated()
-          }
-          loadTokens {
-            val session = sessionStorage.get()
-
-            if (session != null) {
-              BearerTokens(
-                accessToken = session.accessToken,
-                refreshToken = null,
-              )
-            } else {
-              null
-            }
-          }
-        }
-      }
-      install(KtorLogoutPlugin) {
-        this.logoutUseCase = logoutUseCase
-        this.mainEventBus = mainEventBus
-      }
-    }
-  }
-
   @OptIn(ExperimentalTime::class)
   @Provides
   fun provideJson(): Json {
@@ -158,11 +83,3 @@ object AppModule {
 }
 
 typealias LocalDataStore = DataStore<Preferences>
-
-fun HttpRequestBuilder.authenticated() {
-  attributes[AuthenticatedAttributeKey] = true
-}
-
-fun HttpRequestBuilder.isAuthenticated(): Boolean {
-  return attributes.getOrNull(AuthenticatedAttributeKey) == true
-}
---
+++ app/src/main/java/com/example/app/di/KtorModule.kt
@@ -0,0 +1,95 @@
+package com.example.app.di
+
+import com.example.app.MainEventBus
+import com.example.app.data.datastore.SessionStorage
+import com.example.app.domain.LogoutUseCase
+import com.example.app.utils.Logger
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.BearerTokens
+import io.ktor.client.plugins.auth.providers.bearer
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.util.AttributeKey
+import kotlinx.serialization.json.Json
+import javax.inject.Singleton
+import io.ktor.client.plugins.logging.Logger as KtorLogger
+
+private val AuthenticatedAttributeKey = AttributeKey<Boolean>("authenticated-attribute")
+
+@Module
+@InstallIn(SingletonComponent::class)
+object KtorModule {
+  @Provides
+  @Singleton
+  fun provideHttpClient(
+    json: Json,
+    sessionStorage: SessionStorage,
+    logoutUseCase: LogoutUseCase,
+    mainEventBus: MainEventBus,
+    appLogger: Logger,
+  ): HttpClient {
+    return HttpClient {
+      install(Logging) {
+        level = LogLevel.ALL
+        logger = object : KtorLogger {
+          override fun log(message: String) {
+            appLogger.debug("KtorClient") { message }
+          }
+        }
+      }
+      install(ContentNegotiation) {
+        json(
+          json = json,
+          contentType = ContentType.Any
+        )
+      }
+      install(DefaultRequest) {
+        contentType(ContentType.Application.Json)
+        url(urlString = "https://api.baubuddy.de/")
+      }
+      install(Auth) {
+        bearer {
+          // If true, sends the auth bearer header without waiting for 401 response
+          sendWithoutRequest { request ->
+            request.isAuthenticated()
+          }
+          loadTokens {
+            val session = sessionStorage.get()
+
+            if (session != null) {
+              BearerTokens(
+                accessToken = session.accessToken,
+                refreshToken = null,
+              )
+            } else {
+              null
+            }
+          }
+        }
+      }
+      install(KtorLogoutPlugin) {
+        this.logoutUseCase = logoutUseCase
+        this.mainEventBus = mainEventBus
+      }
+    }
+  }
+}
+
+fun HttpRequestBuilder.authenticated() {
+  attributes[AuthenticatedAttributeKey] = true
+}
+
+fun HttpRequestBuilder.isAuthenticated(): Boolean {
+  return attributes.getOrNull(AuthenticatedAttributeKey) == true
+}
